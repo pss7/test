9c78925d39c22c380fabc3636c5a0d21980792cf
pss7 <124497122+pss7@users.noreply.github.com>
2023-02-23 10:14:48 +0900

    Add files via upload

--- admin/js/script.js
+++ admin/js/script.js
@@ -3,12 +3,9 @@ $(function () {
     $('#aside .list1 > li > a').click(function () {
         if ($(this).hasClass('active')) {
             $('#aside .list1 > li > a').removeClass();
-            $('#aside .list2').slideUp();
         } else {
             $('#aside .list1 > li > a').removeClass();
             $(this).addClass('active');
-            $('#aside .list2').slideUp();
-            $(this).next().slideDown();
         }
         return false;
     });
